9e9c0a3a4d435c11a2b104f4594af9c4a8ee5c07
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-02-13 19:54:00 +0600

    adds heading details

--- experiment/test.js
+++ experiment/test.js
@@ -15,7 +15,33 @@ let mainContent = document.querySelector(".main-content");
 let headingElement = document.querySelector(".heading");
 let sidebarElement = document.querySelector(".content-side-bar");
 
-headingElement.innerHTML = restaurant.name;
+// Inserting Resturant name
+headingElement.insertAdjacentHTML(
+  "afterbegin",
+  `<div class="heading-title">\
+    <h1 class="heading-name">${restaurant.name}</h1>
+  </div>`
+);
+
+// Inserting cuisine tag
+let tagMarkup = ``;
+restaurant.highlightedTag.map((item) => {
+  tagMarkup += `<span class="card-tag-${item.highlightType}">${item.text}</span>`;
+});
+tagMarkup = `<p class="card-tags">` + tagMarkup + `</p>`;
+
+headingElement.insertAdjacentHTML("afterbegin", tagMarkup);
+
+// Inserting resturant address and avarage price
+headingElement.insertAdjacentHTML(
+  "beforeend",
+  `<h4 class="card-address">
+    ${restaurant.address.street}, ${restaurant.address.zipCode}, ${restaurant.address.locality}
+  </h4>
+  <p class="card-address">
+    Avarage price range: ${restaurant.acceptedCurrency} ${restaurant.averagePrice}
+  </p>`
+);
 
 function formatOpeningHoursMarkup(openingHoursString) {
   let OpeningHoursArray = openingHoursString
@@ -23,11 +49,10 @@ function formatOpeningHoursMarkup(openingHoursString) {
     .filter((item) => item.length > 0)
     .map((item) => {
       let day,
-        tokens = item.split(",").map((item) => item.trim());
-
-      let times = tokens.map((item) =>
-        item.split("-").map((item) => item.trim())
-      );
+        tokens = item.split(",").map((item) => item.trim()),
+        times = tokens.map((item) =>
+          item.split("-").map((item) => item.trim())
+        );
 
       let final = times.map((item) => {
         let start,
@@ -41,13 +66,42 @@ function formatOpeningHoursMarkup(openingHoursString) {
         }
         return { start, end };
       });
-      return { [day]: final };
+      return { day: day, time: final };
     });
-  return OpeningHoursArray;
+
+  let openingHoursMarkup = ``;
+
+  if (OpeningHoursArray.length > 0) {
+    openingHoursMarkup += `
+    <table class="timetable">
+      <thead>
+        <tr>
+          <th>Day</th>
+          <th>Start time</th>
+          <th>End time</th>
+        </tr>
+      </thead>`;
+    OpeningHoursArray.map((item) => {
+      openingHoursMarkup += `
+      <tr>
+        <td rowspan="3" class="cell-color-day">${item.day}</td>`;
+      item.time.map((it, index) => {
+        if (index > 0) openingHoursMarkup += `<tr>`;
+        openingHoursMarkup += `
+          <td>${it.start}</td>
+          <td>${it.end}</td>
+        </tr>`;
+      });
+    });
+    openingHoursMarkup += `
+    </table>`;
+  }
+
+  return openingHoursMarkup;
 }
 
 let openingHours = formatOpeningHoursMarkup(restaurant.openingHours);
-console.log(openingHours);
+// console.log(openingHours);
 
 // let markupSidebar = `<p>${restaurant.openingHours}</p>`;
-// sidebarElement.insertAdjacentHTML("afterbegin", markupSidebar);
+sidebarElement.insertAdjacentHTML("afterbegin", openingHours);
