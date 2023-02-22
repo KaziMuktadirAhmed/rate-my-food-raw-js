d733004ffe46e18a7d90eabd5fee925017b2a367
KaziMuktadirAhmed <kazisujoy@gmail.com>
2023-02-22 14:02:31 +0600

    update api key

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const RapidAPIKey = "b3998266bcmsh09f23c7d4feabf0p17c888jsn91250d11d863";
+export const RapidAPIKey = "60eef2cca6msh1af4b2bbbcd21f6p10f875jsn584216d18d5f";
 export const RapidAPIHost = "the-fork-the-spoon.p.rapidapi.com";
